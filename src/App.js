ddee4815d450ffe135061dc453df2f98956a2556
NotJordanZA <jordan.bareiss1@gmail.com>
2024-08-18 21:43:32 +0200

    Automatic Deployment Testing
    -changed page to be deployed
    -changed App.test.js to reflect changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Wits Infrastructure Management</h1>
+      <h1>Wits Infrastructure Management 1</h1>
     </div>
   );
 }
