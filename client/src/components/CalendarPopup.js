83cb389ac3a688db4403ae154beaa3769becb96e
NotJordanZA <jordan.bareiss1@gmail.com>
2024-09-03 20:58:54 +0200

    Fixed calendar and buttons
    -fixed the functionality of the popup buttons and the calendar so that they could be used to update the date properly
    
    Co-Authored-By: ruben <84664992+morriscommaruben@users.noreply.github.com>

--- client/src/components/CalendarPopup.js
+++ client/src/components/CalendarPopup.js
@@ -1,14 +1,16 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-function CalendarPopup(){
+function CalendarPopup({ onChange, value }){
     const today = new Date();
     const finalDayofYear = new Date((new Date().getFullYear().toString())+"-12-31");
     return (
         <div>
         <Calendar 
         minDate={today}
-        maxDate={finalDayofYear}/>
+        maxDate={finalDayofYear}
+        onChange={onChange}
+        value={value}/>
         </div>
     );
 }
--- client/src/components/DateHeader.js
+++ client/src/components/DateHeader.js
@@ -8,37 +8,39 @@ function DateHeader(){
     const today = new Date();
     const maxDate = new Date((new Date().getFullYear().toString())+"-12-31");
     
-    const [displayDate, setDisplayDate] = useState(new Date());//maybe getting reset to today when calendar causes page reload
+    const [displayDate, setDisplayDate] = useState(today);
 
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleCalendar = () => setIsOpen(!isOpen);
 
-    const handleDateChange = (direction) => {//this is not working, gets called 4 times when page is loaded
-        console.log("changed date");
+    const handleDateChange = (direction) => {
+        const newDate = new Date(displayDate);
         if(!direction && today < displayDate){
-            console.log(displayDate);
-            setDisplayDate(displayDate-1);
+            newDate.setDate(displayDate.getDate()-1);
         }else if(direction && maxDate > displayDate){
-            console.log(displayDate);
-            setDisplayDate(displayDate+1);
+            newDate.setDate(newDate.getDate()+1);
         }
+        setDisplayDate(newDate);
+    };
+
+    const handleCalendarChange = (date) => {
+        setDisplayDate(date);
+        setIsOpen(false);
     };
 
     return (
         <main className="date-container">
         <div className="date-content">
-            <button className="arrow-button" onClick={handleDateChange(false)}><FontAwesomeIcon icon={faCaretLeft} /></button>
-            <button className="date-button"  onClick = {toggleCalendar}>{displayDate}</button>{/*This is seemingly calling handleDateChange*/}
-            <button className="arrow-button" onClick={handleDateChange(true)}><FontAwesomeIcon icon={faCaretRight} /></button>
+            <button className="arrow-button" onClick={() => handleDateChange(false)}><FontAwesomeIcon icon={faCaretLeft} /></button>
+            <button className="date-button"  onClick = {toggleCalendar}>{displayDate.toDateString()}</button>
+            <button className="arrow-button" onClick={() => handleDateChange(true)}><FontAwesomeIcon icon={faCaretRight} /></button>
         </div>
         {isOpen &&(
             <CalendarPopup
-            // // onChange = {setDisplayDate}
-            // // value = {displayDate}
-            // onClick = {(value) => setDisplayDate(value)}
+            onChange = {handleCalendarChange}
+            value = {displayDate}
             />
-            // This is not updating displayDate
         )}
         </main>
     );
