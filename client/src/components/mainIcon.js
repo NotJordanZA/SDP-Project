34eaba6ee4a154931405973bc49c8952ddc5efc6
NotJordanZA <jordan.bareiss1@gmail.com>
2024-09-09 16:00:17 +0200

    Added routing from dashboard
    -added routing to venues page from
    -updated MainIcon to use divs instead of navs
    -removed logged in user authentication from home for now
    
    Co-Authored-By: ruben <84664992+morriscommaruben@users.noreply.github.com>

--- client/src/components/mainIcon.js
+++ client/src/components/mainIcon.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "../styles/mainIcon.css";
 
-export default function MainIcon({ iconClass, text }) {
+export default function MainIcon({ iconClass, text, onClickFunction}) {
     return (
-        <a>
-            <nav className="main-icon">
-                <i className={iconClass}></i>
-                <p className="icon-text">{text}</p>
-            </nav>
-        </a>
+        <div className="main-icon" onClick={onClickFunction}>
+            <i className={iconClass}></i>
+            <p className="icon-text">{text}</p>
+        </div>
     );
 }
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -7,23 +7,26 @@ import { auth } from "../firebase";
 const HomePage = () => {
 
   const navigate = useNavigate();
-  const user = auth.currentUser;
-  useEffect(() => {
-    if (user === null) {
-      console.log(user);
-      navigate("/login");
-      //console.log("how did we get here?");
-    }
-  }, [user, navigate]); // Effect will run when the user or navigate changes
+  // const user = auth.currentUser;
+  // useEffect(() => {
+  //   if (user === null) {
+  //     console.log(user);
+  //     navigate("/login");
+  //   }
+  // }, [user, navigate]); // Effect will run when the user or navigate changes
 
-  if (user === null) {
-    // Optionally, return null or a loading indicator to avoid rendering content before navigation
-    return null;
+  // if (user === null) {
+  //   // Optionally, return null or a loading indicator to avoid rendering content before navigation
+  //   return null;
+  // }
+
+  const reroute = (path) => {
+    navigate(path);
   }
 
   return (
     <article className="home-page">
-        <MainIcon iconClass="fa-solid fa-house icon-img" text="BOOK A VENUE" />
+        <MainIcon iconClass="fa-solid fa-house icon-img" text="BOOK A VENUE" onClickFunction ={() =>reroute("/venues")}/>
         <MainIcon iconClass="fa-solid fa-calendar icon-img" text="VIEW CALENDAR" />
         <MainIcon iconClass="fa-solid fa-clipboard-check icon-img" text="MY BOOKINGS" />
         <MainIcon iconClass="fa-solid fa-file-alt icon-img" text="FILE A REPORT" /> 
