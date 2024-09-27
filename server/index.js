210b8fadf161bd9db0e725609106dd05d5bf20bb
ruben <2437157@students.wits.ac.za>
2024-09-27 14:00:24 +0200

    fixed failing test + updated env path

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const {onRequest} = require("firebase-functions/v2/https");
 const { getStorage, ref, uploadBytes, getDownloadURL } = require('firebase/storage'); 
 const logger = require("firebase-functions/logger");
-// const PORT = process.env.PORT || 3001; //Must be commented out for production build
-const dotenv = require('dotenv').config({ path: '../.env' });
+const PORT = process.env.PORT || 3001; //Must be commented out for production build
+const dotenv = require('dotenv').config({ path: './.env' });
 
 const app = express();
 app.use(express.json());
@@ -1257,8 +1257,8 @@ app.post('/api/upload-image', upload.single('image'), async (req, res) => {
 
 // Left out for deployment
 // Prints to console the port of the server
-// app.listen(PORT, () => {
-// console.log(`Server listening on ${PORT}`);
-// });
+app.listen(PORT, () => {
+console.log(`Server listening on ${PORT}`);
+});
 
 exports.api = onRequest(app);
