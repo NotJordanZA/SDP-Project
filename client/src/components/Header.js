b66aa111e16d02657434c377ac1f57d9153bd835
Ari921 <ariellalewis10@gmail.com>
2024-10-20 16:15:54 +0200

    red dot

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,15 +6,25 @@ import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase"; 
 import { onAuthStateChanged } from 'firebase/auth';
+import { getNotifications } from '../utils/getNotificationsUtil';
 
 const Header = ({ title, toggleSidebar,toggleNotification }) => {
   const [user, setUser] = useState(null);
+  const [notificationsheader, setNotificationsHeader] = useState([]); //store notifications
+
   const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         setUser(currentUser);
+        getNotifications(currentUser.email, (notificationsData) => {
+          const unreadNotificationsheader = notificationsData.filter(
+            (notification) => !notification.read
+          );
+          setNotificationsHeader(unreadNotificationsheader); //store unread notifications
+        
+        });
       } else {
         setUser(null);
       }
@@ -22,7 +32,8 @@ const Header = ({ title, toggleSidebar,toggleNotification }) => {
 
     return () => unsubscribe();
   }, []);
-
+  
+  
   return (
     <main className="whole-page">
       <header className="app-header">
@@ -36,16 +47,15 @@ const Header = ({ title, toggleSidebar,toggleNotification }) => {
         )}
         <h1 id='title-text' onClick={() => navigate("/home")}>{title}</h1>
         <div className="header-icons">
-          {/* Bell icon for notifications */}
-          {user && (
-            <i 
-            
-              className="fa-solid fa-bell bell-icon" 
-              data-testid="bell-icon"
-              onClick={toggleNotification}
-             // onClick={() => navigate("/notifications")}
-        
-            />
+            {/*notification bell with red dot : this is displayed only when the user has notifications */}
+            {user && (
+            <div className="notificationHeader-notification-container" onClick={toggleNotification}>
+              <i 
+                className="fa-solid fa-bell bell-icon" 
+                data-testid="bell-icon"
+              />
+              {notificationsheader.length > 0 && <span className="notificationHeader-red-dot"></span>}
+            </div>
           )}
           {/* Logo */}
           <img 
